18e58e3bf38d9e486ea826fd393a21a8a23e4eb5
Ed Cooper <egcooper@deloitte.co.uk>
2018-04-23 14:21:33 +0100

    main component shells created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { AdminComponent } from './admin/admin.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { AddUserComponent } from './admin/add-user/add-user.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminComponent,
+    LoginComponent,
+    HomeComponent,
+    AddUserComponent
   ],
   imports: [
     BrowserModule,
